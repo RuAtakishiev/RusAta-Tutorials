c447a9e34a47be060409e2af3b82af2093807937
RuAtakishiev <atakishiev32ruslan@gmail.com>
2022-07-27 20:28:46 +0300

    Added event handling in the feedback form and sending quick messages

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from flask import Flask
 from flask import render_template
-from flask import url_for
+from flask import request
+from flask import flash
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Justkey'
+
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "О сайте", "url": "about"},
         {"name": "Обратная связь", "url": "contact"}]
 
 @app.route("/")
 def index():
-    print(url_for('index'))
     return render_template('index.html', menu=menu)
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='О сайте', menu=menu)
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
+    if request.method == 'POST':
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено',  category='success')
+        else:
+            flash('Ошибка отправки',  category='error')
+
     return render_template('contact.html', title='Обратная связь', menu=menu)
 
 if __name__ == "__main__":
